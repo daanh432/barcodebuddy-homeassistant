61506a6179cfebdc7b018379e028db9402ab2018
daanh432 <8281824+daanh432@users.noreply.github.com>
2022-08-17 17:23:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ trap cleanup EXIT
 if [ ! -d "/data/config" ] 
 then
     echo "Directory /data/config DOES NOT exists." 
+    echo "Syncing between /data/config and /config"
+    mkdir /data/config
+    /usr/bin/unison -batch -auto -silent -log -logfile /config/unison.log /data/config /config
 else
     echo "Directory /data/config DOES exists."
     echo "Syncing between /data/config and /config"
