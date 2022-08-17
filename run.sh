5e035d92c684c98e4ef1af970b111805abd5fac3
daanh432 <8281824+daanh432@users.noreply.github.com>
2022-08-17 16:49:05 +0200

    Move original directory

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-mkdir /data/config
+if [ ! -d "/data/config" ] 
+then
+    echo "Directory /data/config DOES NOT exists." 
+    mv /config /data/config
+else
+    rm -rf /config
+fi
 ln -sn /data/config /config
 /init
