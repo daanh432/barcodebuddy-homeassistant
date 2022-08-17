3ad104dc0a4d0d26412947346d566cd24c3208f7
daanh432 <8281824+daanh432@users.noreply.github.com>
2022-08-17 17:04:11 +0200

    Sync with copies instead

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+function cleanup {
+  echo "Syncing /config to /data/config\n"
+  cp -r /config/* /data/config/
+}
+
+trap cleanup EXIT
+
 if [ ! -d "/data/config" ] 
 then
-    echo "Directory /data/config DOES NOT exists." 
-    mv /config /data/config
+    echo "Directory /data/config DOES NOT exists.\n" 
 else
-    rm -rf /config
+    echo "Directory /data/config DOES exists.\n"
+    echo "Syncing /data/config to /config\n"
+    cp -r /data/config/* /config/
 fi
-ln -sn /data/config /config
 /init
+
